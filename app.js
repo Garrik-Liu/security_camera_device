18ab19fd7baf5f10aa6fad0d4e86d3ccd41838b9
Garrik-Liu <garrikliu@Gmail.com>
2018-11-03 17:09:18 -0400

    update

--- app.js
+++ app.js
@@ -111,7 +111,7 @@ function postPicture(name) {
 }
 
 function test() {
-    request('https://google.ca', function(error, response, body) {
+    request(serverUrl, function(error, response, body) {
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     });
