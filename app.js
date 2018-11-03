f7e52c43b4fb5293aed7e21e6c9be9a6991e410e
Garrik-Liu <garrikliu@Gmail.com>
2018-11-03 17:08:55 -0400

    update

--- app.js
+++ app.js
@@ -114,6 +114,5 @@ function test() {
     request('https://google.ca', function(error, response, body) {
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
     });
 }
