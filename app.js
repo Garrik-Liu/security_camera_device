cdfa1e4226649dc97cfc521a0862c82166b3f816
Garrik-Liu <garrikliu@Gmail.com>
2018-11-03 17:08:22 -0400

    update

--- app.js
+++ app.js
@@ -111,7 +111,7 @@ function postPicture(name) {
 }
 
 function test() {
-    request(serverUrl, function(error, response, body) {
+    request('https://google.ca', function(error, response, body) {
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         console.log('body:', body); // Print the HTML for the Google homepage.
