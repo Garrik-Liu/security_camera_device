7bb96fa82725077fe61b206ef4131795069ca733
Garrik-Liu <garrikliu@Gmail.com>
2018-11-04 10:54:43 -0500

    update

--- app.js
+++ app.js
@@ -13,7 +13,45 @@ const echo = new Gpio(24, { mode: Gpio.INPUT, alert: true });
 
 trigger.digitalWrite(0); // Make sure trigger is low
 
-const watchHCSR04 = () => {
+watchHCSR04();
+
+setInterval(() => {
+    trigger.trigger(10, 1); // Set trigger high for 10 microseconds
+}, 200);
+
+function takePicture(name) {
+    exec("fswebcam " + __dirname + "/snapshots/" + name + ".png", (err, stdout, stderr) => {
+        if (err) {
+            return console.error(err);
+        }
+        console.log('Taking Picture Successful!');
+
+        postPicture(name)
+    })
+
+}
+
+function postPicture(name) {
+    var formData = {
+        image: fs.createReadStream(__dirname + '/snapshots/' + name + '.png'),
+    };
+
+    console.log(serverUrl + 'images/add');
+
+    request.post({ url: serverUrl + 'images/add', formData: formData }, function optionalCallback(err, response, body) {
+        if (err) {
+            return console.error(err);
+        }
+
+        console.log('error:', err); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log(body);
+
+
+    });
+}
+
+function watchHCSR04() {
     let startTick;
     let initialDistance;
 
@@ -35,6 +73,7 @@ const watchHCSR04 = () => {
 
                     if (diff > 5) {
                         let dateStr = new Date().toISOString();
+
                         takePicture(dateStr);
                     }
                 }
@@ -42,38 +81,3 @@ const watchHCSR04 = () => {
         }
     });
 };
-
-watchHCSR04();
-
-setInterval(() => {
-    trigger.trigger(10, 1); // Set trigger high for 10 microseconds
-}, 200);
-
-function takePicture(name) {
-    exec("fswebcam " + __dirname + "/snapshots/" + name + ".png", (err, stdout, stderr) => {
-        if (err) {
-            return console.error(err);
-        }
-        console.log('Taking Picture Successful!');
-        postPicture(name)
-    })
-
-}
-
-function postPicture(name) {
-    var formData = {
-        image: fs.createReadStream(__dirname + '/snapshots/' + name + '.png'),
-    };
-    console.log(serverUrl + 'images/add');
-    request.post({ url: serverUrl + 'images/add', formData: formData }, function optionalCallback(err, response, body) {
-        if (err) {
-            return console.error(err);
-        }
-
-        console.log('error:', err); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log(body);
-
-
-    });
-}
