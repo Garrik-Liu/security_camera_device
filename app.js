e29ce405921838daa85409c254f77040e188f43e
Garrik-Liu <garrikliu@Gmail.com>
2018-11-06 08:02:08 -0500

    update

--- app.js
+++ app.js
@@ -107,6 +107,7 @@ function watchHCSR04() {
                 } else {
                     let diff = Math.abs(distance - prevDistance);
                     prevDistance = distance;
+                    console.log(diff)
                     if (diff > 10) {
                         console.log('A motion is detected.');
 
