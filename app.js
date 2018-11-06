5c7027baa3c92437768108ab419372563d08fc88
Garrik-Liu <garrikliu@Gmail.com>
2018-11-06 07:49:23 -0500

    update

--- app.js
+++ app.js
@@ -83,7 +83,6 @@ function postPicture(name) {
         if (err) {
             return console.error(err);
         }
-
         console.log('error:', err); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         console.log(body);
