e06f378f2d36f5793fc1e93d3a98f3bcbb543d9f
Garrik-Liu <garrikliu@Gmail.com>
2018-11-19 21:06:06 -0500

    update

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ board.on("ready", function() {
         }
     });
 
-    let streamProcess = exec(
+    exec(
         "ffmpeg -f v4l2 -framerate 30 -video_size 640x360 " +
         "-i /dev/video0 -f mpegts -codec:v mpeg1video -b:v 1800k -r 30 " +
         CONFIG.StreamServerUrl +
@@ -77,7 +77,7 @@ board.on("ready", function() {
     }, 200);
 
     this.on("exit", function() {
-        streamProcess.kill();
+
     });
 });
 
