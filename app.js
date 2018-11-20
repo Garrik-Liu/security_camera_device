c33598fb241a785fa8fdcc95c972fe8248523502
Garrik-Liu <garrikliu@Gmail.com>
2018-11-19 22:16:05 -0500

    update

--- app.js
+++ app.js
@@ -42,10 +42,6 @@ board.on("ready", function() {
         }
     );
 
-    streamProcess.on('exit', function(code) {
-        console.log('STREAM 子进程已关闭，代码：' + code);
-    });
-
     setInterval(() => {
         exec('ls ' + __dirname + '/snapshots -l | grep "^-" | wc -l', (err, stdout, stderr) => {
             if (err) {
