1b84a5c1ba4661f1463df11c29100edcee8d8ac7
Garrik-Liu <garrikliu@Gmail.com>
2018-11-24 13:38:42 -0500

    update

--- app.js
+++ app.js
@@ -18,68 +18,68 @@ let cameraInfo = {
     status: 'off'
 }
 
-const board = new five.Board({
-    io: new Raspi()
-});
+// const board = new five.Board({
+//     io: new Raspi()
+// });
 
-board.on("ready", function() {
-    const led = new five.Led("GPIO20");
-    const motionSensor = new five.Motion("GPIO21");
+// board.on("ready", function() {
+//     const led = new five.Led("GPIO20");
+//     const motionSensor = new five.Motion("GPIO21");
 
-    const detectObj = {
-        motion: false,
-        prevTime: null
-    }
+//     const detectObj = {
+//         motion: false,
+//         prevTime: null
+//     }
 
-    let streamProcess = null;
+//     let streamProcess = null;
 
-    motionSensor.on("data", function(data) {
-        if (data.detectedMotion && !detectObj.motion) {
-            console.log('A motion is detected')
-            detectObj.motion = true;
-            detectObj.prevTime = new Date();
-        }
-    });
+//     motionSensor.on("data", function(data) {
+//         if (data.detectedMotion && !detectObj.motion) {
+//             console.log('A motion is detected')
+//             detectObj.motion = true;
+//             detectObj.prevTime = new Date();
+//         }
+//     });
 
-    // setInterval(() => {
-    //     exec('ls ' + __dirname + '/snapshots -l | grep "^-" | wc -l', (err, stdout, stderr) => {
-    //         if (err) {
-    //             return console.error(err);
-    //         }
+//     // setInterval(() => {
+//     //     exec('ls ' + __dirname + '/snapshots -l | grep "^-" | wc -l', (err, stdout, stderr) => {
+//     //         if (err) {
+//     //             return console.error(err);
+//     //         }
 
-    //         let count = Number(stdout);
+//     //         let count = Number(stdout);
 
-    //         if (detectObj.motion) {
+//     //         if (detectObj.motion) {
 
-    //             exec('ls -Art ' + __dirname + '/snapshots | tail -n 1', (err, stdout, stderr) => {
-    //                 if (err) {
-    //                     return console.error(err);
-    //                 }
+//     //             exec('ls -Art ' + __dirname + '/snapshots | tail -n 1', (err, stdout, stderr) => {
+//     //                 if (err) {
+//     //                     return console.error(err);
+//     //                 }
 
-    //                 let filename = stdout;
+//     //                 let filename = stdout;
 
-    //                 if ((new Date() - detectObj.prevTime) >= CONFIG.Interval) {
-    //                     postPicture(filename.trim());
-    //                     detectObj.motion = false;
-    //                 }
-    //             })
-    //         }
+//     //                 if ((new Date() - detectObj.prevTime) >= CONFIG.Interval) {
+//     //                     postPicture(filename.trim());
+//     //                     detectObj.motion = false;
+//     //                 }
+//     //             })
+//     //         }
 
-    //         if ((count > 5 && !detectObj.motion) || count > 10) {
-    //             console.log('delete snapshots')
-    //             exec('sudo rm ' + __dirname + '/snapshots/*.png', (err, stdout, stderr) => {
-    //                 if (err) {
-    //                     return console.error(err);
-    //                 }
-    //             })
-    //         }
-    //     });
-    // }, 200);
+//     //         if ((count > 5 && !detectObj.motion) || count > 10) {
+//     //             console.log('delete snapshots')
+//     //             exec('sudo rm ' + __dirname + '/snapshots/*.png', (err, stdout, stderr) => {
+//     //                 if (err) {
+//     //                     return console.error(err);
+//     //                 }
+//     //             })
+//     //         }
+//     //     });
+//     // }, 200);
 
-    this.on("exit", function() {
-        streamProcess.kill();
-    });
-});
+//     this.on("exit", function() {
+//         streamProcess.kill();
+//     });
+// });
 
 socket.on('turnOn camera', function() {
     console.log('turn on');
